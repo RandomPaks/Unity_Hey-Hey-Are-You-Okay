fed787ce20fbc2b69659ed21887f90f61e7b267f
Paolo Roxas <paoloroxas1@gmail.com>
2021-09-01 08:00:08 +0800

    swipe goal

---
+++ Hey Hey Are You Okay/Assets/Scripts/Gameplay/Goals/SwipeGoal.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+
+public class SwipeGoal : MonoBehaviour
+{
+    [SerializeField] GameObject[] goals;
+    [SerializeField] GameObject tool;
+    [SerializeField] string eventToPlay;
+
+    void Update()
+    {
+        if (goals[0].GetComponent<Collider2D>().IsTouching(tool.GetComponent<Collider2D>()))
+        {
+            goals[1].SetActive(true);
+        }
+        if (goals[1].GetComponent<Collider2D>().IsTouching(tool.GetComponent<Collider2D>()))
+        {
+            goals[0].SetActive(false);
+            goals[1].SetActive(false);
+            GameManager.Instance.FinishedSwipeEvent(eventToPlay);
+        }
+    }
+}
