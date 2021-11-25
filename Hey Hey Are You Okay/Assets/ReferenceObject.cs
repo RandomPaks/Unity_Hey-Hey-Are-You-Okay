aa497f05ce2f44a28bd8390e02137f8c11131b72
Paolo Roxas <paoloroxas1@gmail.com>
2021-11-25 18:17:57 +0800

    added accuracy

---
+++ Hey Hey Are You Okay/Assets/ReferenceObject.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ReferenceObject : MonoBehaviour
+{
+    public BezierSplineObjects splineObjects;
+    public ReferenceObject goal;
+    public ReferenceObject mistake;
+    public ReferenceObject mistake2;
+
+    public void OnTriggerGoal()
+    {
+        splineObjects.correctNum++;
+        splineObjects.RemoveObjects();
+
+        Destroy(goal.gameObject);
+        Destroy(mistake.gameObject);
+        Destroy(mistake2.gameObject);
+
+        splineObjects.ActivateObjects();
+    }
+
+    public void OnTriggerMistake()
+    {
+        splineObjects.RemoveObjects();
+
+        Destroy(goal.gameObject);
+        Destroy(mistake.gameObject);
+        Destroy(mistake2.gameObject);
+
+        splineObjects.ActivateObjects();
+    }
+}
---
+++ Hey Hey Are You Okay/Assets/Scripts/TouchManager.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TouchManager : MonoBehaviour
+{
+    public GameObject gameObject;
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    void FixedUpdate()
+    {
+        if (Input.GetButton("Fire1"))
+        {
+            int layerMask = 1 << 30;
+
+            Vector3 mousePosFar = new Vector3(Input.mousePosition.x, Input.mousePosition.y, Camera.main.farClipPlane);
+            Vector3 mousePosNear = new Vector3(Input.mousePosition.x, Input.mousePosition.y, Camera.main.nearClipPlane);
+            Vector3 far = Camera.main.ScreenToWorldPoint(mousePosFar);
+            Vector3 near = Camera.main.ScreenToWorldPoint(mousePosNear);
+
+            RaycastHit hit;
+            if (Physics.Raycast(near, far - near, out hit, Camera.main.farClipPlane, layerMask))
+            {
+                Vector3 collide = Vector3.Normalize(far - near) * hit.distance;
+                Debug.DrawRay(near, collide, Color.green);
+
+                gameObject.transform.position = hit.point;
+            }
+            else
+            {
+                Debug.DrawRay(near, far - near, Color.green);
+            }
+        }
+    }
+}
