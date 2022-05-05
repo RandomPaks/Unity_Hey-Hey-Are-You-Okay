843954f3582d8079b22e93daac8d95a109bc18c8
Paolo Roxas <paoloroxas1@gmail.com>
2022-05-05 21:22:28 +0800

    made some fixes i want to rebuild this game from scratch :(

--- Hey Hey Are You Okay/Assets/Scripts/Menu/SceneLoader.cs
+++ Hey Hey Are You Okay/Assets/Scripts/Menu/SceneLoader.cs
@@ -1,9 +1,21 @@
 ﻿using System.Collections;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class SceneLoader : MonoBehaviour
 {
+    [SerializeField] Image lobbyButtonImage;
+    [SerializeField] Sprite lobbyButtonSprite;
+
+    void Start()
+    {
+        if (PersistentManager.Instance.isExam || PersistentManager.Instance.isSurvival)
+        {
+            lobbyButtonImage.sprite = lobbyButtonSprite;
+        }
+    }
+
     //Resolves restart button issues
     public void LoadScene(string scene)
     {
