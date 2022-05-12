bc267ae772ee05b228171c8fbcb0a0ff48b31ea9
Paolo Roxas <paoloroxas1@gmail.com>
2022-05-12 17:48:04 +0800

    hold goal now has rub

--- Hey Hey Are You Okay/Assets/_Scripts/Gameplay/Goals/HoldGoal.cs
+++ Hey Hey Are You Okay/Assets/_Scripts/Gameplay/Goals/HoldGoal.cs
@@ -7,14 +7,23 @@ public class HoldGoal : MonoBehaviour
     [SerializeField] ToolEnum goalTool;
     [SerializeField] string eventToPlay;
     [SerializeField] float progressSpeed = 0.5f;
-
-    public bool isProgressing;
+    [SerializeField] bool isGoalRub = false;
 
     void OnTriggerStay(Collider other)
     {
         if (GameManager.Instance.currentTool != null && GameManager.Instance.currentTool.tool == goalTool)
         {
-            GameManager.Instance.Progress(eventToPlay, progressSpeed);
+            if (isGoalRub)
+            {
+                if (GameManager.Instance.currentTool.ToolSpeed > 0.1f)
+                {
+                    GameManager.Instance.Progress(eventToPlay, progressSpeed);
+                }
+            }
+            else
+            {
+                GameManager.Instance.Progress(eventToPlay, progressSpeed);
+            }
         }
         else if (GameManager.Instance.currentTool != null && GameManager.Instance.currentTool.tool != goalTool)
         {
--- Hey Hey Are You Okay/Assets/_Scripts/Gameplay/ToolDrag.cs
+++ Hey Hey Are You Okay/Assets/_Scripts/Gameplay/ToolDrag.cs
@@ -24,10 +24,24 @@ public class ToolDrag : MonoBehaviour, IDragHandler, IEndDragHandler
     public ToolEnum tool;
     PointerEventData lastPointerData;
 
+
     Vector3 startPosition;
-    void Start()
+    Vector3 lastPosition = Vector3.zero;
+
+    private float _toolSpeed;
+
+    public float ToolSpeed => _toolSpeed;
+
+    private void Start()
     {
         startPosition = gameObject.transform.position;
+    }
+
+    private void FixedUpdate()
+    {
+        _toolSpeed = (transform.position - lastPosition).magnitude;
+        lastPosition = transform.position;
+        Debug.Log(_toolSpeed);
     }
 
     public void OnDrag(PointerEventData eventData)
